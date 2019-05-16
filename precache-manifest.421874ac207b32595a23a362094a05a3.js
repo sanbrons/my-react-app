743c787f887aae7812754c0ec73187f72a04f9d3
Sanbron Liong <sanbronliong@Sanbrons-MacBook-Air.local>
2019-05-16 12:14:56 +0800

    Updates

---
+++ precache-manifest.421874ac207b32595a23a362094a05a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1562242479b09c7bffd82b807f515cf5",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "1926168503d7481c71a3",
+    "url": "/my-react-app/static/css/main.de2e29a4.chunk.css"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/my-react-app/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "1926168503d7481c71a3",
+    "url": "/my-react-app/static/js/main.bc1a1e2d.chunk.js"
+  },
+  {
+    "revision": "b28cb091c153ab1cf392",
+    "url": "/my-react-app/static/js/runtime~main.f5ab8794.js"
+  }
+]);
